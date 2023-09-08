b943d9dbd33e45940820b9fcacd69e8a39415754
dkokonkwo <d.okonkwo@alustudent.com>
2023-09-08 19:21:24 +0200

    test1

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+""" 
+unittests for BaseModel class
+"""
+import os
+import unittest
 from models.base_model import BaseModel
+from datetime import datetime
+
 
 my_model = BaseModel()
 my_model.name = "My First Model"
 my_model.my_number = 89
-print(my_model)
 my_model.save()
 print(my_model)
 my_model_json = my_model.to_dict()

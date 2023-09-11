d4de3e00c677c597efe66c6f81e1641c31b1ede0
dkokonkwo <d.okonkwo@alustudent.com>
2023-09-11 16:35:44 +0200

    datetime

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from datetime import datetime
-from models import storage
+# from models import storage
 import uuid
 
 
@@ -19,14 +19,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
 
     def __str__(self) -> str:
         return (f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}')
 
     def save(self):
         self.updated_at = datetime.now()
-        storage.save()
+        # storage.save()
 
     def to_dict(self):
         upd_dict = {
@@ -35,3 +35,16 @@ class BaseModel:
             '__class__': self.__class__.__name__,
         }
         return {**self.__dict__, **upd_dict}
+
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
